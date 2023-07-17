1906fb7ae960da0673901f77b5a9bb9c68953d86
Suraj Kamal <surajkamal@cloudwaveinc.com>
2023-07-17 23:56:11 +0530

    Add linked lists & doubly linked lists problems

---
+++ DSA-problems/src/doublyLinkedList/Main.java
@@ -0,0 +1,15 @@
+package doublyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList myDLL = new DoublyLinkedList(0);
+        myDLL.append(1);
+        myDLL.append(2);
+
+        myDLL.printList();
+
+        myDLL.remove(1);
+
+       myDLL.printList();
+    }
+}
