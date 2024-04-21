dde94cacd5f024fe91dc15b44820e8faa0fad6b0
Suraj Kamal <surajkamal60@gmail.com>
2024-04-21 10:09:49 +0530

    LC contest

--- DSA-problems/src/contest/leetcide210424/CountTheNumberOfSpecialCharactersI.java
+++ DSA-problems/src/contest/leetcide210424/CountTheNumberOfSpecialCharactersI.java
@@ -1,20 +1,32 @@
 package contest.leetcide210424;
 
+import java.util.Arrays;
+
 public class CountTheNumberOfSpecialCharactersI {
     public int numberOfSpecialChars(String word) {
-        int[] freqLC = new int[26];
-        int[] freqUC = new int[26];
+        char[] freq = new char[26];
+        Arrays.fill(freq, 'N');
         int count = 0;
         for (char ch : word.toCharArray()) {
             if (ch >= 97 && ch <= 122) {
-                freqLC[ch - 97]++;
+                if (freq[ch - 97] == 'N') {
+                    freq[ch - 97] = 'L';
+                }
+                if (freq[ch - 97] == 'U') {
+                    freq[ch - 97] = 'B';
+                }
             }
             if (ch >= 65 && ch <= 90) {
-                freqUC[ch - 65]++;
+                if (freq[ch - 65] == 'N') {
+                    freq[ch - 65] = 'U';
+                }
+                if (freq[ch - 65] == 'L') {
+                    freq[ch - 65] = 'B';
+                }
             }
         }
         for (int i = 0; i < 26; i++) {
-            if (freqLC[i] > 0 && freqUC[i] > 0) {
+            if (freq[i] == 'B') {
                 count++;
             }
         }
