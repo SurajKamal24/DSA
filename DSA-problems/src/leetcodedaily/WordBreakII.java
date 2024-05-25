c06727c05d0257b2d5246085e97186f0a283f3a8
Suraj Kamal <surajkamal60@gmail.com>
2024-05-25 22:05:20 +0530

    lc daily

---
+++ DSA-problems/src/leetcodedaily/WordBreakII.java
@@ -0,0 +1,30 @@
+package leetcodedaily;
+
+import java.util.*;
+
+public class WordBreakII {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        return helper(s,0,set);
+    }
+    private List<String> helper(String str, int s, Set<String> set){
+        List<String> valid = new ArrayList<>();
+        if(s == str.length()){
+            valid.add("");
+        }
+        for(int e = s+1; e <= str.length(); e++){
+            String pre = str.substring(s, e);
+            if(set.contains(pre)){
+                List<String> suff = helper(str, e, set);
+                for(String suf : suff){
+                    valid.add(pre + (suf.equals("") ? "" : " ")+ suf);
+                }
+            }
+        }
+        return valid;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new WordBreakII().wordBreak("catsanddog", List.of("cat", "cats", "and", "sand", "dog")).toString());
+    }
+}
