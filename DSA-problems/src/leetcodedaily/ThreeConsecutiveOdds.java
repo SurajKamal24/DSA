fcd775877192b03ddeec8ffec391ed98d6b5e55e
Suraj Kamal <surajkamal60@gmail.com>
2024-07-01 13:06:05 +0530

    lc daily

---
+++ DSA-problems/src/leetcodedaily/ThreeConsecutiveOdds.java
@@ -0,0 +1,23 @@
+package leetcodedaily;
+
+public class ThreeConsecutiveOdds {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 1) {
+                count++;
+                if (count == 3) {
+                    return true;
+                }
+            }
+            else {
+                count = 0;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ThreeConsecutiveOdds().threeConsecutiveOdds(new int[]{2,6,4,1}));
+    }
+}
