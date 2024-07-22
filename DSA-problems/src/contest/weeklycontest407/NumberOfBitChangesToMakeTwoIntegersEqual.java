604ac8b9fe90e278dd6da8faf29a939b629d8c09
Suraj Kamal <surajkamal60@gmail.com>
2024-07-22 17:36:15 +0530

    contest problems

--- DSA-problems/src/contest/weeklycontest407/NumberOfBitChangesToMakeTwoIntegersEqual.java
+++ DSA-problems/src/contest/weeklycontest407/NumberOfBitChangesToMakeTwoIntegersEqual.java
@@ -1,30 +1,30 @@
 package contest.weeklycontest407;
 
+import java.util.Arrays;
+
 public class NumberOfBitChangesToMakeTwoIntegersEqual {
     public int minChanges(int n, int k) {
-        public int minChanges(int n, int k) {
-            String binaryN = Integer.toBinaryString(n);
-            String binaryK = Integer.toBinaryString(k);
-            int maxLen = Math.max(binaryN.length(), binaryK.length());
-            binaryN = String.format("%" + maxLen + "s", binaryN).replace(" ", "0");
-            binaryK = String.format("%" + maxLen + "s", binaryK).replace(" ", "0");
-            int onesInN = 0, minChanges = 0;
-            for (int i = 0; i < maxLen; i++) {
-                if (binaryN.charAt(i) == '1') {
-                    onesInN++;
-                    if (binaryK.charAt(i) == '0') {
-                        minChanges++;
-                    }
-                }
-                if (binaryN.charAt(i) == '0' and binaryK.charAt(i) == '1') {
-                    return -1;
+        String binaryN = Integer.toBinaryString(n);
+        String binaryK = Integer.toBinaryString(k);
+        int maxLen = Math.max(binaryN.length(), binaryK.length());
+        binaryN = String.format("%" + maxLen + "s", binaryN).replace(" ", "0");
+        binaryK = String.format("%" + maxLen + "s", binaryK).replace(" ", "0");
+        int onesInN = 0, minChanges = 0;
+        for (int i = 0; i < maxLen; i++) {
+            if (binaryN.charAt(i) == '1') {
+                onesInN++;
+                if (binaryK.charAt(i) == '0') {
+                    minChanges++;
                 }
             }
-            if (onesInN > minChanges) {
+            if (binaryN.charAt(i) == '0' && binaryK.charAt(i) == '1') {
                 return -1;
             }
-            return minChanges;
         }
+        if (minChanges > onesInN) {
+            return -1;
+        }
+        return minChanges;
     }
     public static void main(String[] args) {
         System.out.println(new NumberOfBitChangesToMakeTwoIntegersEqual().minChanges(14, 13));
