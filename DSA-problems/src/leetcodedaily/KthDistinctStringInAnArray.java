31e372cd67652cd54fb5818690f1cdf8102c2d13
Suraj Kamal <surajkamal60@gmail.com>
2024-08-05 11:29:52 +0530

    lc daily

---
+++ DSA-problems/src/leetcodedaily/KthDistinctStringInAnArray.java
@@ -0,0 +1,25 @@
+package leetcodedaily;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KthDistinctStringInAnArray {
+    public String kthDistinct(String[] arr, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : arr) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        for (String s : arr) {
+            if (map.get(s) == 1) {
+                k--;
+                if (k == 0) {
+                    return s;
+                }
+            }
+        }
+        return "";
+    }
+    public static void main(String[] args) {
+        System.out.println(new KthDistinctStringInAnArray().kthDistinct(new String[] {}));
+    }
+}
